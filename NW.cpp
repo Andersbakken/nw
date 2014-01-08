0de9999d0bd8c72222c0405fa337d86a7affb56a
Anders Bakken <agbakken@gmail.com>
2014-01-07 22:02:36 -0800

    More wip

--- NW.cpp
+++ NW.cpp
@@ -1,12 +1,16 @@
 #include "NW.h"
+#include <map>
 #include <assert.h>
 #include <errno.h>
 #include <signal.h>
+#include <stdlib.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/un.h>
+#include <netinet/in.h>
 
 #ifdef NDEBUG
 #define CHECK(op) op
@@ -18,10 +22,10 @@
     } while (0)
 #endif
 
-#define EINTRWRAP(op)                           \
+#define EINTRWRAP(var, op)                      \
     do {                                        \
-        ret = op;                               \
-    } while (ret == -1 && errno == EINTR);
+        var = op;                               \
+    } while (var == -1 && errno == EINTR);
 
 struct LockScope
 {
@@ -94,17 +98,30 @@ int NW::Connection::read(char *buf, int max)
 
 }
 
-bool NW::exec()
+NW::Connection::Connection(int socket, const NW::Interface &local, const NW::Interface &remote)
+    : mSocket(socket), mLocalInterface(local), mRemoteInterface(remote), mBuffer(0), mBufferLength(0)
+{}
+
+NW::Connection::~Connection()
+{
+    if (mBuffer)
+        ::free(mBuffer);
+}
+
+NW::Error NW::exec()
 {
     {
         LockScope scope(mMutex);
         assert(!mRunning);
+        mThread = pthread_self();
         mRunning = true;
     }
 
-    std::vector<std::pair<Interface, ::socket> > interfaces;
+    NW::Error error = Success;
+
+    std::map<int, Connection*> connections;
+    std::map<int, Interface> sockets;
     while (true) {
-        fd_set r, w;
         {
             LockScope scope(mMutex);
             if (!mRunning) {
@@ -113,37 +130,152 @@ bool NW::exec()
             }
             if (mInterfacesDirty) {
                 mInterfacesDirty = false;
-                std::vector<std::pair<Interface, int> >::const_iterator end = interfaces.end();
-                std::vector<std::pair<Interface, int> >::const_iterator it = interfaces.begin();
-                while (it != end) {
-                    if (it->second)
-                        close(it->second);
-                    ++it;
-                }
+                // do I need to close all current connections on interfaces that
+                // have disappeared?
 
-                interfaces = mInterfaces;
+                std::map<int, Interface>::iterator it = sockets.begin();
+                while (it != sockets.end()) {
+                    ::close(it->first);
+                    sockets.erase(it++);
+                }
+                int idx = 0;
+                for (std::vector<Interface>::const_iterator it = mInterfaces.begin(); it != mInterfaces.end(); ++it) {
+                    const int fd = ::socket(AF_INET, SOCK_STREAM, 0);
+                    if (fd < 0) {
+                        char buf[128];
+                        snprintf(buf, sizeof(buf), "::socket failed %d", errno);
+                        log(Error, buf);
+                        error = SocketInit;
+                        break;
+                    }
+
+                    int reuseaddr = 1;
+                    if (::setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int))) {
+                        char buf[128];
+                        snprintf(buf, sizeof(buf), "::setsockopt failed %d", errno);
+                        log(Error, buf);
+                        ::close(fd);
+                        error = SocketInit;
+                        break;
+                    }
+
+                    sockaddr_in address;
+                    ::memset(&address, 0, sizeof(address));
+                    address.sin_family = AF_INET;
+                    address.sin_port = htons(it->port);
+                    if (it->ip == IP::any()) {
+                        address.sin_addr.s_addr = INADDR_ANY;
+                    } else {
+                        address.sin_addr.s_addr = htonl((it->ip.a << 24) | (it->ip.b << 16) | (it->ip.c << 8) | it->ip.d);
+                    }
+
+                    if (::bind(fd, reinterpret_cast<sockaddr*>(&address), sizeof(address))) {
+                        ::close(fd);
+                        char buf[128];
+                        snprintf(buf, sizeof(buf), "::bind failed %d", errno);
+                        log(Error, buf);
+                        error = SocketInit;
+                        break;
+                    }
+                    if (::listen(fd, it->backlog)) {
+                        ::close(fd);
+                        char buf[128];
+                        snprintf(buf, sizeof(buf), "::listen failed %d", errno);
+                        log(Error, buf);
+                        error = SocketInit;
+                        break;
+                    }
+                }
+                if (error)
+                    break;
             }
         }
-        FD_ZERO
+        fd_set r, w;
+        FD_ZERO(&r);
+        FD_ZERO(&w);
+        FD_SET(mPipe[0], &r);
+        int max = mPipe[0];
+        for (std::map<int, Interface>::const_iterator it = sockets.begin(); it != sockets.end(); ++it) {
+            FD_SET(it->first, &r);
+            FD_SET(it->first, &w);
+            max = std::max(max, it->first);
+        }
+        int count = 0;
+        EINTRWRAP(count, ::select(max, &r, &w, 0, 0));
+        assert(count > 0);
+        char pipe = 0;
+        if (FD_ISSET(mPipe[0], &r)) {
+            int written = 0;
+            EINTRWRAP(written, ::read(mPipe[0], &pipe, sizeof(pipe)));
+            --count;
+        }
+        for (std::map<int, Interface>::const_iterator it = sockets.begin(); count && it != sockets.end(); ++it) {
+            // uint8_t mode = 0;
+            // if (FD_ISSET(it->first, &r)) {
+            //     mode |= Read;
+            // } else if (FD_ISSET(it->first, &w)) {
+            //     mode |= Write;
+            // }
+            if (FD_ISSET(it->first, &r)) {
+                --count;
+                acceptConnection(it->first, it->second);
+            }
+        }
     }
-}
+    {
+        for (std::map<int, Interface>::const_iterator it = sockets.begin(); it != sockets.end(); ++it) {
+            ::close(it->first);
+        }
+    }
 
-void * NW::thread(void *userData)
-{
-    assert(userData);
-    NW *self = reinterpret_cast<NW*>(userData);
-    self->exec();
-    return 0;
+    {
+        for (std::map<int, Connection*>::const_iterator it = connections.begin(); it != connections.end(); ++it) {
+            ::close(it->first);
+            delete it->second;
+        }
+    }
+
+
+    return error;
 }
 
 void NW::wakeup(char byte)
 {
     int ret;
-    EINTRWRAP(write(mPipe[1], &byte, sizeof(byte)));
+    EINTRWRAP(ret, write(mPipe[1], &byte, sizeof(byte)));
+    assert(ret == 1);
+    (void)ret;
 }
 
-std::vector<Interface> NW::interfaces() const
+std::vector<NW::Interface> NW::interfaces() const
 {
     LockScope scope(mMutex);
     return mInterfaces;
+}
+
+void NW::processConnection(Connection *connection)
+{
+
+}
+
+void NW::acceptConnection(int fd, const Interface &localInterface)
+{
+    sockaddr address;
+    socklen_t len;
+    int fd;
+    EINTRWRAP(fd, ::accept(listener->sock, &address, &len));
+    if (fd < 0) {
+        char buf[128];
+        snprintf(buf, sizeof(buf), "::accept failed %d", errno);
+        log(Error, buf);
+        return;
+    }
+
+    conn->request_info.remote_port = ntohs(conn->client.rsa.u.sin.sin_port);
+    memcpy(&conn->request_info.remote_ip,
+           &conn->client.rsa.u.sin.sin_addr.s_addr, 4);
+    conn->request_info.remote_ip = ntohl(conn->request_info.remote_ip);
+
+    Connection (
+
 }
--- NW.h
+++ NW.h
@@ -1,10 +1,10 @@
 #ifndef NW_h
 #define NW_h
 
-#include <map>
 #include <vector>
 #include <string>
 #include <pthread.h>
+#include <stdint.h>
 
 class NW
 {
@@ -12,10 +12,29 @@ public:
     NW();
     virtual ~NW();
 
+    struct IP
+    {
+        IP(unsigned char aa = 0, unsigned char bb = 0, unsigned char cc = 0, unsigned dd = 0)
+            : a(aa), b(bb), c(cc), d(dd)
+        {}
+
+        static IP localhost() { return IP(127, 0, 0, 1); }
+        static IP any() { return IP(); }
+
+        bool operator==(const IP &o) const { return (a == o.a && b == o.b && c == o.c && d == o.d); }
+        bool operator!=(const IP &o) const { return (a != o.a || b != o.b || c != o.c || d != o.d); }
+
+        unsigned char a, b, c, d;
+    };
+
     struct Interface
     {
-        std::string host; // empty means all
-        int port;
+        Interface()
+            : ip(IP::any()), port(0), backlog(128)
+        {}
+
+        IP ip;
+        uint16_t port, backlog;
     };
     void setInterface(const Interface &interface)
     {
@@ -24,25 +43,52 @@ public:
     void setInterfaces(const std::vector<Interface> &interfaces);
     std::vector<Interface> interfaces() const;
 
-    bool exec();
+    enum Error {
+        Success,
+        SocketInit,
+    };
+    Error exec();
     void stop();
 
-    struct Connection
+    class Connection
     {
-        int socket;
-        std::vector<std::string> headers;
+    public:
+        int socket() const { return mSocket; }
+        Interface localInterface() const { return mLocalInterface; }
+        Interface remoteInterface() const { return mRemoteInterface; }
+        const std::vector<std::string> &headers() const { return mHeaders; }
 
         int contentLength() const;
         int read(char *buf, int max);
+    private:
+        Connection(int socket, const Interface &local, const Interface &remote);
+        ~Connection();
+
+        int mSocket;
+        Interface mLocalInterface, mRemoteInterface;
+
+        std::vector<std::string> mHeaders;
+        char *mBuffer;
+        int mBufferLength;
+        friend class NW;
     };
 
     virtual void handleConnection(Connection *conn) = 0;
-
+    enum LogType {
+        Debug,
+        Error
+    };
+    virtual void log(LogType, const char *) {}
 private:
+    enum Mode {
+        Read = 0x1,
+        Write = 0x2
+    };
+
+    void acceptConnection(int fd, const Interface &localInterface);
+    void processConnection(Connection *connection);
     void wakeup(char byte);
-    static void *thread(void *);
-    void lock();
-    void unlock();
+
     bool mRunning;
     mutable pthread_mutex_t mMutex;
     pthread_t mThread;
