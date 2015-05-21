89943a7947841292934525cbf07096b11e8f89aa
Anders Bakken <agbakken@gmail.com>
2015-05-21 12:21:41 -0700

    More work.

--- lib/ws.cmake
+++ lib/ws.cmake
@@ -1,3 +1,4 @@
 cmake_minimum_required(VERSION 2.8)
-include_directories(${CMAKE_CURRENT_LIST_DIR})
+include_directories(${CMAKE_CURRENT_LIST_DIR} ${WSLAY_INCLUDE})
 add_library(nw ${NW_LIBRARY_MODE} ${CMAKE_CURRENT_LIST_DIR}/NW.cpp)
+target_link_libraries(nw ${WSLAY_LIBS})
